0c733b77476b5d6ec20c505911f7ea7021f3de11
rricol <92322566+rricol@users.noreply.github.com>
2024-02-25 15:31:47 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: *,
+    origin: 'https://dev.kissmyhack.ch',
     methods: 'GET',
     credentials: false,
   });

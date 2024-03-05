f7a765f135819b28bff2f23dbaf9c00c8b9ecfb8
rricol <92322566+rricol@users.noreply.github.com>
2024-03-05 15:07:52 +0100

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,7 +8,7 @@ export class AppService {
       'https://outlook.office365.com/owa/calendar/cb667b0fc3c64d00ac29c54350fc0304@upsa-vaud.ch/0685cc3456eb4029a13dcc5cb2507003941587491522089634/calendar.ics',
       'https://outlook.office365.com/owa/calendar/cb667b0fc3c64d00ac29c54350fc0304@upsa-vaud.ch/4d8e3809c662407788d7bd53a491891017821783700213908280/calendar.ics',
       'https://outlook.office365.com/owa/calendar/cb667b0fc3c64d00ac29c54350fc0304@upsa-vaud.ch/39834768f9f64608bccc710cecf9053511768132098882597266/calendar.ics',
-      'https://outlook.office365.com/owa/calendar/cb667b0fc3c64d00ac29c54350fc0304@upsa-vaud.ch/39834768f9f64608bccc710cecf9053511768132098882597266/calendar.ics', // TODO duplicate manque électricité 201
+      'https://outlook.office365.com/owa/calendar/cb667b0fc3c64d00ac29c54350fc0304@upsa-vaud.ch/f97106ce83574d6d8fd5a77c7d405d435737247495643173665/calendar.ics', // TODO duplicate manque électricité 201
       'https://outlook.office365.com/owa/calendar/cb667b0fc3c64d00ac29c54350fc0304@upsa-vaud.ch/e9a49e2f711c4a418bed0a916ec8509b15230699364362141759/calendar.ics',
       'https://outlook.office365.com/owa/calendar/cb667b0fc3c64d00ac29c54350fc0304@upsa-vaud.ch/27efba0d21af47d4805cdca333a6ec347063493297512780677/calendar.ics',
       'https://outlook.office365.com/owa/calendar/cb667b0fc3c64d00ac29c54350fc0304@upsa-vaud.ch/d4c6669fe82d49e88afbada23c0d60a42169409082601815304/calendar.ics',
